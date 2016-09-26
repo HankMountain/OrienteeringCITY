28a42643aae6a7e724eeb5ef2f2f958c3825dd87
HankMountain <walk.in.sky100@gmail.com>
2016-09-26 21:16:21 +0800

    add Firebase test

---
+++ Firebase/FirebaseTEST/FirebaseTEST/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseTEST
+//
+//  Created by Hank on 2016/9/26.
+//  Copyright © 2016年 Hank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
